4b410f221e2b20ee814238ea2e4fbf6506c626fe
KDF5000 <kdf5000@163.com>
2015-09-01 17:04:30 +0800

    kongdefei:初始化

---
+++ src/TpLogger/Logger.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KDF5000
+ * Date: 2015/9/1
+ * Time: 15:18
+ */
+
+namespace TpLogger;
+use Monolog\Logger as MonoLogger;
+use Monolog\Handler\StreamHandler;
+
+use Monolog\Handler\HandlerInterface;
+use ReflectionClass;
+
+class Logger{
+
+    const DEFAULT_HANLDER = 'StreamHandler'; //默认的Handler
+    const DEFAULT_PATH = 'my_log.log';  //默认log存储文件
+
+    const DEBUG = MonoLogger::DEBUG;
+    const INFO = MonoLogger::INFO;
+    const NOTICE = MonoLogger::NOTICE;
+    const WARNING = MonoLogger::WARNING;
+    const ERROR = MonoLogger::ERROR;
+    const CRITICAL = MonoLogger::CRITICAL;
+    const ALERT = MonoLogger::ALERT;
+    const EMERGENCY = MonoLogger::EMERGENCY;
+
+    /**
+     * @var HandlerInterface
+     */
+    static private $handler;
+    /**
+     * @var \Monolog\Logger
+     */
+    static protected $logger = null;
+
+    /**
+     * @param array $config
+     * @param string $logName
+     * example:
+     * @see \Monolog\Handler
+     * array = array(
+     *     "StreamHandler" => array(
+     *            'filename' => 'my_log.log',
+     *            'level' => Logger::Debug,
+     *
+     *     ),
+     *     "RotatingFileHandler" => array(
+     *            'filename' => 'webapp_log',
+     *            'max_file' => 0,
+     *            'level' => Logger::Debug,
+     *     ),
+     *     "SwiftMailerHander" => array(
+     *            'level' => Logger::Debug,
+     *            'mailer'=> SwiftMailer,
+     *     )
+     *     ...
+     * )
+     */
+    public static function init(array $config=array(),$logName='my_log'){
+        if(self::$logger == null){
+            self::$logger = new MonoLogger($logName);
+        }
+        if(empty($config)){
+            self::$logger->pushHandler(new StreamHandler(self::DEFAULT_PATH, self::DEBUG));
+        }else{
+            //通过配置自动设置
+            foreach($config as $key => $val){
+                $class = new ReflectionClass('Monolog\\Handler\\'.$key);
+                $handler = $class->newInstanceArgs(array_values($val));
+                self::$logger->pushHandler($handler);
+            }
+        }
+    }
+
+    /**
+     * @param $message
+     */
+    public static function addDebug($message){
+        self::$logger->addDebug($message);
+    }
+
+    /**
+     * @param $message
+     */
+    public static function addError($message){
+        self::$logger->addError($message);
+    }
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boss
+ * Date: 2015/9/1
+ * Time: 15:40
+ */
+
+require '../vendor/autoload.php';
+
+use TpLogger\Logger;
+
+$logger = new Logger();
+$logger->init(array(
+    "StreamHandler" => array(
+        'filename' => 'my_log_stream.log',
+        'level' => Logger::DEBUG,
+    ),
+    "RotatingFileHandler" => array(
+        'filename' => 'my_log_rotating.log',
+        'maxfiles' => 0,
+        'level' => Logger::ERROR,
+    ),
+));
+$logger->addDebug('hello');
+$logger->addError('i am an error');
+//use Monolog\Logger;
+//use Monolog\Handler\StreamHandler;
+//
+//$logger = new Logger('test');
+//$logger->pushHandler(new StreamHandler('demo.txt'), Logger::DEBUG);
+//
+//$logger->addDebug('nihao');
